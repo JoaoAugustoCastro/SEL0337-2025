ee137c3a593a9fb3d4ac0ffc14bfd63ba38ac9a1
JoaoAugustoCastro <j0ao.castro@usp.br>
2025-11-24 13:30:48 +0000

    atualiza blink.sh comentado

--- blink.sh
+++ blink.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/bash  
 
-echo 18 > /sys/class/gpio/export
-echo out > /sys/class/gpio/gpio18/direction
+#primeira linha inicializa interpretaçao em bash
+echo 18 > /sys/class/gpio/export  # inicializa a  pinagem do gpio 18
+echo out > /sys/class/gpio/gpio18/direction # seta o direcionamento da gpio 18 como saída
 
-while [ 1 ]
+while [ 1 ] # loop infinito
 
  do
-   echo 1 > /sys/class/gpio/gpio18/value
-   sleep 0.2s
-   echo 0 > /sys/class/gpio/gpio18/value
-   sleep 0.2s
- done
+   echo 1 > /sys/class/gpio/gpio18/value # envia valor 1 (alto) para gpio 18
+   sleep 0.2s # aguarda 0,2 segundos
+   echo 0 > /sys/class/gpio/gpio18/value # envia valor 0 (baixo) para gpio 18
+   sleep 0.2s # aguarda 0,2 segundos
+ done #encerra loop
 
